2b9642a4f8a9168e72172df3aaf9eba288dbddc1
NataliaKlimczyk <klimczyk.natalia@gmail.com>
2018-08-26 10:33:49 +0200

    My Django Girls app, first commit

---
+++ workshop.py
@@ -0,0 +1,52 @@
+# print('Hello, Django girls!')
+# a = 6
+# b ="6"
+# a + int(b)
+# print(a + int(b))
+# if 5>2:    print("To działa")
+# if 2>1:
+#     print('to nie działa')
+#     print('makapkaak')
+# # if 1>3:
+# #     print ('dziala')
+# # else:
+# #     print('nie dziala')
+# volume = 50
+# if volume < 10:
+#     print('blad')
+# elif volume < 20:
+#     print('blad2')
+# elif volume < 100:
+#     print('poprawnie')
+# else:
+#     print ('czesc')
+# # #sprawdzic czy poprawnie
+# def hi():
+#     print ('Hej!')
+#     print('jak sie masz?')
+#
+# hi()
+#
+# def hi(imie):
+#     if imie =="Natalia":
+#         print("Hej Natalia!")
+#     elif imie =="Kasia":
+#         print("Hej Kasia!")
+#     else:
+#          print("nieznany")
+# hi("Kasia")
+#
+# def hi(imie):
+#     print("Hej"+ imie + "!")
+# hi("Ola")
+
+# def hi(imie):
+#     print("Hi " + imie + "!")
+# dziewczyny=["Ola","Kasia","Ewa"]
+# for imie in dziewczyny:
+#     hi(imie)
+#     print('Kolejne dziewczyny')
+# for i in range(1,6):
+#     print(i)
+for i in range(0,301,50):
+    print(i)
